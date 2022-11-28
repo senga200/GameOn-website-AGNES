46aab0320ac206180e8e16dfab984b477c4cf2e1
senga <senga.ds@gmail.com>
2022-11-28 18:17:55 +0100

    AJOUT ALERTES VERTES ET ROUGES SOUS LES CHAMPS

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -23,19 +23,37 @@ const birthdate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 const checkCG = document.querySelector(".checkbox-icon");
 const checkbox1 = document.getElementById("checkbox1");
-const checkboxError = document.getElementById("checkboxError");
 
+const champValide= document.getElementsByClassName("champValide");
 const submit = document.getElementById("btn-submit");
 
 
 submit.addEventListener('click',btnValid);
+//submit ?
 
-function btnValid(e){
 
-//PRENOM MANQUANT - FORMAT INCORRECT
+//CONST CHAMPS MANQUANTS -- CONST REGEX 
+
 const prenomManquant = document.getElementById("prenomManquant");
 const prenomRegex = /^[a-zA-Z][a-zéèêçîï]+([-'\s][a-zA-Z][a-zéèêçîï]+)?/;
 
+const nomManquant = document.getElementById("nomManquant");
+const nomRegex = /^[a-zA-Z][a-zéèêçîï]+([-'\s][a-zA-Z][a-zéèêçîï]+)?/;
+
+const mailManquant = document.getElementById("mailManquant");
+const mailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+const quantiteManquante = document.getElementById("quantiteManquante");
+const quantiteRegex = /^[1-9]?[0-9]{1}$|^100$/;
+
+const checkboxError = document.getElementById("checkboxError");
+
+
+
+
+function btnValid(e){
+
+//PRENOM MANQUANT - FORMAT INCORRECT - CHAMP VALIDE
   if (first.validity.valueMissing){
     e.preventDefault();
     prenomManquant.textContent = "Merci de renseigner votre prénom";  
@@ -44,12 +62,13 @@ const prenomRegex = /^[a-zA-Z][a-zéèêçîï]+([-'\s][a-zA-Z][a-zéèêçîï]
     e.preventDefault();
     prenomManquant.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom";
     prenomManquant.style.color ="red";
+  } else {
+    prenomManquant.textContent ="Prénom Valide";
+    prenomManquant.style.color ="green";
+    return true;
   }
 
-    //NOM MANQUANT - FORMAT INCORRECT
-  const nomManquant = document.getElementById("nomManquant");
-  const nomRegex = /^[a-zA-Z][a-zéèêçîï]+([-'\s][a-zA-Z][a-zéèêçîï]+)?/;
-
+//NOM MANQUANT - FORMAT INCORRECT - CHAMP VALIDE
     if (last.validity.valueMissing){
       e.preventDefault();
       nomManquant.textContent = "Merci de renseigner votre nom";  
@@ -58,12 +77,13 @@ const prenomRegex = /^[a-zA-Z][a-zéèêçîï]+([-'\s][a-zA-Z][a-zéèêçîï]
       e.preventDefault();
       nomManquant.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
       nomManquant.style.color ="red";
+    } else {
+      nomManquant.textContent ="nom Valide";
+      nomManquant.style.color ="green";
+      return true;
     }
 
-      //EMAIL MANQUANT - FORMAT INCORRECT
-  const mailManquant = document.getElementById("mailManquant");
-  const mailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
+//EMAIL MANQUANT - FORMAT INCORRECT - CHAMP VALIDE
     if (email.validity.valueMissing){
       e.preventDefault();
       mailManquant.textContent = "Merci de renseigner votre e-mail";  
@@ -72,6 +92,11 @@ const prenomRegex = /^[a-zA-Z][a-zéèêçîï]+([-'\s][a-zA-Z][a-zéèêçîï]
       e.preventDefault();
       mailManquant.textContent = "format incorrect";
       mailManquant.style.color ="red";
+    } else {
+      mailManquant.textContent ="e-mail Valide";
+      mailManquant.style.color ="green";
+      return true;
+
     }
 
     //         //DATE DE NAISSANCE MANQUANTE - FORMAT INCORRECT
@@ -88,10 +113,7 @@ const prenomRegex = /^[a-zA-Z][a-zéèêçîï]+([-'\s][a-zA-Z][a-zéèêçîï]
     //     dateManquante.style.color ="red";
     //   }
 
-            //QUANTITE MANQUANTE - FORMAT INCORRECT
-  const quantiteManquante = document.getElementById("quantiteManquante");
-  const quantiteRegex = /^[1-9]?[0-9]{1}$|^100$/;
-
+//QUANTITE MANQUANTE - FORMAT INCORRECT - CHAMP VALIDE
     if (quantity.validity.valueMissing){
       e.preventDefault();
       quantiteManquante.textContent = "Merci de compléter le nombre de tournois";  
@@ -100,206 +122,27 @@ const prenomRegex = /^[a-zA-Z][a-zéèêçîï]+([-'\s][a-zA-Z][a-zéèêçîï]
       e.preventDefault();
       quantiteManquante.textContent = "format incorrect";
       quantiteManquante.style.color ="red";
+    } else {
+      quantiteManquante.textContent ="quantité Valide";
+      quantiteManquante.style.color ="green";
+      return true;
+
     }
 
-          //CG CHECK 
-  const checkboxError = document.getElementById("checkboxError");;
-       
+//CG CHECK - UNCHECKED - CHECKED       
     if (checkbox1.checked== false){
       e.preventDefault();
       checkboxError.textContent = "Vous devez vérifier que vous acceptez les termes et conditions.";  
       checkboxError.style.color ="red";
     } else { 
-      alert("merci")}
+      checkboxError.textContent = "Vous avez accepté les conditions d'utilisation";
+      checkboxError.style.color ="green";
+      return true;
+    }
+
+    // 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// inscription.addEventListener("submit", e =>{
-//   e.preventDefault();
-  
-//   inputsValides();
-// });
-
-
-// //ERREUR CHAMP VIDE 
-// const erreurChamp = (element, message) => {
-//   const inputControl = element.parentElement;
-//   const messageErreur = inputControl.querySelector(".erreur");
-
-//   messageErreur.innerHTML = "Veuillez entrer votre nom";
-//   inputControl.classList.add("erreur");
-//   inputControl.classList.remove("succes");
-// }
-
-
-// const champValide = element => {
-//   const inputControl = element.parentElement;
-//   const messageErreur = inputControl.querySelector(".erreur");
-
-//   messageErreur.innerText = "";
-//   inputControl.classList.add("succes");
-//   inputControl.classList.remove("erreur");
-// }
-
-
-
-
-
-
-
-
-
-// const emailValide = email => {
-//   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   return re.test(String(email).toLowerCase());
-// }
-
-// // const dateValide = date => {
-// //   const regex = /(0[1-9]|1[1-9]|2[0-9]|3[01]).(0[1-9]|1[012]).([19]{2})?([1-9]{2})/;
-// //   return regex.test(String(date).toLowerCase());
-// // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//let checkboxValidation = true;
-
-
-
-// const inputsValides =() =>{
-//   const firstValue = first.value.trim();
-//   const lastValue = last.value.trim();
-//   const emailValue = email.value.trim();
-//   const birthdateValue = birthdate.value.trim();
-//   const quantityValue = quantity.value.trim();
-//   const checkCGValue = checkCG.value.trim();
-
-//     if(firstValue === "") {
-//       erreurChamp(first, "Veuillez renseigner votre prénom");
-//     } else if(firstValue.length < 2){
-//       erreurChamp(first, "Votre prénom doit comporter au moins 2 caractères");
-//     } else {
-//        champValide(first);
-//     }
-    
-//     if(lastValue === "") {
-//       erreurChamp(last, "Veuillez renseigner votre nom");
-//     } else if(lastValue.length < 2){
-//       erreurChamp(first, "Votre nom doit comporter au moins 2 caractères");
-//     }else {
-//       champValide(last);
-//     }
-
-//     if(emailValue === "") {
-//       erreurChamp(email, "Veuillez renseigner votre email");
-//     } else if(!emailValide(emailValue)){
-//       erreurChamp(email, "Veuillez renseigner un email valide");
-//     } else {
-//       champValide(email);
-//     }
-
-//      if(birthdateValue === "") {
-//      erreurChamp(birthdate, "Veuillez renseigner votre date de naissance");
-//     } else if(!birthdateValue(birthdateValue)){
-//       erreurChamp(birthdate, "Veuillez renseigner votre date de naissance");
-//     } else {
-//       champValide(birthdate);
-//     }
-
-//     if(quantityValue ==""){
-//       erreurChamp(quantity, "Veuillez renseigner votre nom");
-//     }else {
-//       champValide(last);
-//     }
-
-//     if(!checkCGValue.checked){
-//       checkboxError.innerHTML = "Les conditions doivent être acceptés.";
-//     checkboxError.classList.add("errorCheckbox");
-//     checkboxValidation = false;
-//   } else {
-//     checkboxError.innerHTML = "";
-//     checkboxError.classList.remove("errorCheckbox");
-//     checkboxValidation = true;
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//MESSAGES D ERREUR PERSONNALISES 
-// if (first == ""){
-// first.setCustomValidity("votre nom comporte plus de 2 lettres");
-// } else {
-//   alert('formulaire envoyé');
-// }
-
-
-// first.addEventListener(function(e){
-//   if(first.validity.typeMismatch){
-//     first.setCustomValidity("votre nom");
-//   }else {
-//     first.setCustomValidity("");
-//   }
-// });
-
-///////////////////
-
-// fist.oninvalid = function(e) {
-// 	e.target.setCustomValidity("");
-// 	if (!e.target.validity.valid) {
-// 		if (e.target.value.length == 0) {
-// e.target.setCustomValidity("Ce champ est obligatoire");
-// 		} else {
-// e.target.setCustomValidity("Entrez une adresse valide. Exemple : contact@nom.com");
-// 		}
-// 	}
-// };
-////////////////////////
-
-// if (!first.value){
-//   erreur = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-// }
-
-// if (erreur) {
-//   e.preventDefault();
-//   erreur.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
-// } else {
-// alert('formulaire envoyé');
-// }
 
 //FERMER LA FENETRE FORM AVEC X
 closeBtn.onclick = function(){
@@ -319,7 +162,6 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalBg.style.display = "block";
 }
-
 
 
 
